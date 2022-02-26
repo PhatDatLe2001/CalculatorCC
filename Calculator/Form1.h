eb3510bab1cde78afbfcd6f942878e52b139a0e9
SabertoothSBT <phatdatvn2001@gmail.com>
2022-02-26 14:17:00 +0700

    Them giao dien

--- Calculator/Form1.h
+++ Calculator/Form1.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace Calculator {
 
@@ -34,6 +34,14 @@ namespace Calculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected: 
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  txtSoA;
+	private: System::Windows::Forms::TextBox^  txtSoB;
+	private: System::Windows::Forms::TextBox^  txtKQ;
+	private: System::Windows::Forms::Button^  btCong;
 
 	private:
 		/// <summary>
@@ -48,11 +56,89 @@ namespace Calculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->txtSoA = (gcnew System::Windows::Forms::TextBox());
+			this->txtSoB = (gcnew System::Windows::Forms::TextBox());
+			this->txtKQ = (gcnew System::Windows::Forms::TextBox());
+			this->btCong = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(22, 56);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(41, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Số a:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(22, 119);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(41, 17);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Số b:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(22, 191);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(61, 17);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Kết quả:";
+			// 
+			// txtSoA
+			// 
+			this->txtSoA->Location = System::Drawing::Point(69, 51);
+			this->txtSoA->Name = L"txtSoA";
+			this->txtSoA->Size = System::Drawing::Size(165, 22);
+			this->txtSoA->TabIndex = 1;
+			// 
+			// txtSoB
+			// 
+			this->txtSoB->Location = System::Drawing::Point(69, 114);
+			this->txtSoB->Name = L"txtSoB";
+			this->txtSoB->Size = System::Drawing::Size(165, 22);
+			this->txtSoB->TabIndex = 1;
+			// 
+			// txtKQ
+			// 
+			this->txtKQ->Location = System::Drawing::Point(89, 186);
+			this->txtKQ->Name = L"txtKQ";
+			this->txtKQ->Size = System::Drawing::Size(145, 22);
+			this->txtKQ->TabIndex = 1;
+			// 
+			// btCong
+			// 
+			this->btCong->Location = System::Drawing::Point(159, 244);
+			this->btCong->Name = L"btCong";
+			this->btCong->Size = System::Drawing::Size(75, 23);
+			this->btCong->TabIndex = 2;
+			this->btCong->Text = L"Cộng";
+			this->btCong->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(246, 295);
+			this->Controls->Add(this->btCong);
+			this->Controls->Add(this->txtKQ);
+			this->Controls->Add(this->txtSoB);
+			this->Controls->Add(this->txtSoA);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
